ebbb8abac7fc2cca2884859a1ab7d3f053786429
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-11-04 20:58:40 +0800

    新增Db库，添加管理员用户数据库操作

---
+++ Db/DataType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db
+{
+    /////
+    ///     管理员-数据类型
+    /////
+    
+    /////
+    ///     用户-数据类型
+    /////
+
+}
